f66a61fdd70f10892192063986aa0ca9bdfadc3c
csiegal2001 <csiegal2001@g.educaand.es>
2024-06-20 07:15:38 +0200

    Final2

--- data.js
+++ data.js
@@ -103,8 +103,22 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": -0.013092578658664422,
+          "pitch": 0.19290725178389678,
+          "rotation": 0,
+          "target": "0-escena"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1215299697556329,
+          "pitch": -0.3331571016449111,
+          "title": "Tarjeta Grafica",
+          "text": "Una <strong>tarjeta gráfica</strong>, también conocida como GPU (Graphics Processing Unit), es un componente de los ordenadores que se encarga de procesar y generar las imágenes que se muestran en la pantalla. Su función principal es convertir los datos en señales que el monitor pueda entender para mostrar gráficos, videos, y cualquier contenido visual. Las tarjetas gráficas son especialmente importantes para tareas que requieren mucho poder de procesamiento de imágenes, como los videojuegos, la edición de video y el diseño gráfico. A diferencia de la unidad central de procesamiento (CPU), la GPU está optimizada para realizar muchas operaciones en paralelo, lo que la hace mucho más eficiente para manejar gráficos complejos y renderizados 3D."
+        }
+      ]
     },
     {
       "id": "2-gs__0052",
@@ -134,8 +148,22 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": -0.03637589772697325,
+          "pitch": 0.18243278536652596,
+          "rotation": 0,
+          "target": "0-escena"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08859074379476084,
+          "pitch": -0.2407551491869171,
+          "title": "Memoria Ram",
+          "text": "La <strong>memoria RAM (Random Access Memory)</strong> es un tipo de memoria de la computadora que se usa para almacenar datos de forma temporal mientras se están utilizando. Es como el \"espacio de trabajo\" del sistema, donde la computadora guarda información que necesita acceder rápidamente. Por ejemplo, cuando abres un programa o un archivo, se carga en la RAM para que el procesador pueda acceder a él de manera rápida y eficiente. A diferencia del disco duro, la RAM es mucho más rápida, pero también es volátil, lo que significa que pierde toda su información cuando la computadora se apaga. Más memoria RAM permite a la computadora realizar más tareas simultáneamente sin disminuir su velocidad."
+        }
+      ]
     },
     {
       "id": "3-gs__0053",
@@ -165,8 +193,22 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": -0.08638707552536928,
+          "pitch": 0.2656795783032848,
+          "rotation": 0,
+          "target": "0-escena"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2583440428033832,
+          "pitch": -0.3743525581745626,
+          "title": "Title",
+          "text": "Un <strong>disco duro HDD (Hard Disk Drive)</strong> es un dispositivo de almacenamiento de datos que se encuentra en la mayoría de las ordenadores. Funciona utilizando discos metálicos que giran rápidamente, y un pequeño brazo mecánico que lee y escribe información en estos discos. Piensa en él como un tocadiscos donde los discos son las partes que almacenan la música, pero en este caso, almacenan datos de la computadora, como archivos, fotos, videos y programas. Los HDDs son conocidos por su capacidad de almacenar una gran cantidad de información a un costo razonable, aunque son más lentos y tienen partes móviles que pueden desgastarse con el tiempo."
+        }
+      ]
     }
   ],
   "name": "Project Title",
