93733d6d0f8f05848a891444cbd4d494eefec743
csiegal2001 <csiegal2001@g.educaand.es>
2024-06-20 06:39:33 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-escena",
+      "name": "Escena",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.020075216506317,
+        "pitch": 0.23286312169041068,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.439877073683946,
+          "pitch": 0.24911464941869532,
+          "title": "Disco Duro HDD",
+          "text": "<div class=\"flex flex-grow flex-col max-w-full\"><div data-message-author-role=\"assistant\" data-message-id=\"a87197f7-8e15-4efc-9ed5-17332aaff290\" dir=\"auto\" class=\"min-h-[20px] text-message flex flex-col items-start whitespace-pre-wrap break-words [.text-message+&amp;]:mt-5 juice:w-full juice:items-end overflow-x-auto gap-2\"><div class=\"flex w-full flex-col gap-1 juice:empty:hidden juice:first:pt-[3px]\"><div class=\"markdown prose w-full break-words dark:prose-invert dark\"><p>Un disco duro HDD (Hard Disk Drive) es un dispositivo de almacenamiento de datos que se encuentra en la mayoría de las computadoras. Funciona utilizando discos metálicos que giran rápidamente, y un pequeño brazo mecánico que lee y escribe información en estos discos.<br></p><p><br></p><p>Los HDDs son conocidos por su gran capacidad de almacenamiento y su costo relativamente bajo en comparación con otras tecnologías de almacenamiento como las unidades de estado sólido (SSD). Sin embargo, son más lentos y tienen partes móviles, lo que los hace más susceptibles a daños mecánicos.</p></div></div></div></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
